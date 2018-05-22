7addbbb2d0fe8d6baeee65299ea971df8b7699df
curiositypaths <curiositypaths@users.noreply.github.com>
2018-05-22 08:45:38 -0400

    Add starter functions for code organization lecture

--- 31-code-organization/index.js
+++ 31-code-organization/index.js
@@ -1,6 +1,28 @@
 // the module pattern
 
 // list of functions to interact with our API
+function parseResponseJson(response) {
+    return response.json();
+  }
+
+  function messages() {
+    const url =
+      "https://fetch-message-in-the-bottle.herokuapp.com/api/v1/messages";
+    return fetch(url).then(parseResponseJson);
+  }
+
+  function createMessage(body) {
+    const url =
+      "https://fetch-message-in-the-bottle.herokuapp.com/api/v1/messages";
+    const postConfig = {
+      method: "POST",
+      headers: {
+        "Content-type": "application/json"
+      },
+      body: JSON.stringify(body)
+    };
+    return fetch(url, postConfig).then(parseResponseJson);
+  }
 
 // Adapter object
 
